bc0c71334895d62b8361a54a0ad316abfc6b9517
localghost <zkostrzewa@gmail.com>
2016-01-03 12:39:43 +0100

    Remove deploy script

--- src/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rsync --exclude="*.pyc" -avz registry/ arwmar@codeleet.com:/home/arwmar/rails/budget/registry/
-ssh arwmar@codeleet.com "touch /home/arwmar/rails/budget/tmp/restart.txt"
